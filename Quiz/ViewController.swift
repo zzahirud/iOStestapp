578c6cf55a4d6e6c654b3cae3791fcdec80871cc
zubair <contactzuby@gmail.com>
2017-11-12 23:18:37 -0500

    "adding alll files"

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -9,17 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+    
+    @IBOutlet var questionLabel : UILabel!
+    @IBOutlet var answerLabel : UILabel!
+    
+    let questions: [String] = [
+        "What is 7+7 " ,
+        "What is the capital of Mass ?",
+        "When is the Big Day ?"
+    
+    ]
+    
+    let answers : [String] = [
+        "14",
+        "Boston",
+        "in 2 Months"
+    
+    
+    ]
+    
+    var currentQuestionIndex: Int = 0 ;
+    
+    
+    
+    
+    @IBAction func showNextQuestion(_sender : UIButton){
+        currentQuestionIndex = currentQuestionIndex + 1;
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0;
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+        
+        
+        
+    }
+    
+    @IBAction func showAnswer(_sender : UIButton){
+        let answer : String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+   
 
 }
 
